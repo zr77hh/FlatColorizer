61cf1e02f289e00dd43ff24246a7198ee3ce9a7c
muhammed zencir <76841804+zr77hh@users.noreply.github.com>
2023-04-23 23:13:11 +0300

    Mesh UV and texture Generation

--- Assets/_Project/Code/FlatColorizer.cs
+++ Assets/_Project/Code/FlatColorizer.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEditor;
 using UnityEngine;
 
 public class FlatColorizer : MonoBehaviour
@@ -9,23 +5,39 @@ public class FlatColorizer : MonoBehaviour
     [SerializeField] private Color[] _color;
 
     private MeshRenderer _renderer;
+    private MeshFilter _meshFilter;
 
     private void AddMaterial()
     {
-        Material sharedMat = FlatColorizerManager.GetSharedMaterial();
-
         if (_renderer == null)
             _renderer = GetComponent<MeshRenderer>();
 
+
+        Material sharedMat = FlatColorizerManager.GetSharedMaterial();
+
         if (_renderer.sharedMaterial != sharedMat)
-            _renderer.sharedMaterial = sharedMat;
+            _renderer.sharedMaterials = new Material[] { sharedMat };
     }
 
-   
+    private void AddMesh()
+    {
+        if (_meshFilter == null)
+            _meshFilter = GetComponent<MeshFilter>();
+
+        Mesh flatColoredMesh = FlatColorizerManager.GetFlatColorizedMesh(_meshFilter.sharedMesh);
+        if (_meshFilter.sharedMesh != flatColoredMesh)
+        {
+            _meshFilter.sharedMesh = flatColoredMesh;
+            for (int i = 0; i < flatColoredMesh.uv.Length; i++)
+            {
+                Debug.Log(flatColoredMesh.uv[i]);
+            }
+        }
+    }
 
     public void FlatColorize()
     {
         AddMaterial();
-
+        AddMesh();
     }
 }
--- Assets/_Project/Code/FlatColorizerManager.cs
+++ Assets/_Project/Code/FlatColorizerManager.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public static class FlatColorizerManager
 {
+    private const string FLAT_COLORED = "FlatColored_";
+
+
     private static Material CreateMaterial()
     {
         // Create the folder path
@@ -21,13 +24,80 @@ public static class FlatColorizerManager
         // Create a new instance of the material
         Material newMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
 
+        newMaterial.mainTexture = GetTexture();
+
         // Save the material to a file
         string materialPath = $"{folderPath}/SharedMat.mat";
         AssetDatabase.CreateAsset(newMaterial, materialPath);
 
         // Refresh the asset database to ensure the new material appears in the project view
         AssetDatabase.Refresh();
+
+        Debug.Log("new material created");
         return newMaterial;
+    }
+
+    private static Texture2D CreateTexture()
+    {
+        // Create the folder path
+        string folderPath = "Assets/Resources/FlatColorizer/Texture";
+
+        // Check if the folder exists, and create it if it doesn't
+        if (!AssetDatabase.IsValidFolder(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+            AssetDatabase.Refresh();
+        }
+
+
+        Texture2D texture = new Texture2D(512, 512);
+
+        // Set the colors of the texture
+        texture.SetPixel(0, 0, Color.red);
+        texture.SetPixel(5, 6, Color.green);
+
+        // Apply the changes to the texture
+        texture.Apply();
+
+
+        string texturePath = $"{folderPath}/Texture.asset";
+        AssetDatabase.CreateAsset(texture, texturePath);
+
+        // Refresh the asset database to ensure the new material appears in the project view
+        AssetDatabase.Refresh();
+
+        Debug.Log("new texture created");
+
+        return texture;
+    }
+
+    private static Mesh CreateFlatColoredMesh(Mesh mesh)
+    {
+        // Create the folder path
+        string folderPath = "Assets/Resources/FlatColorizer/Mesh";
+
+        // Check if the folder exists, and create it if it doesn't
+        if (!AssetDatabase.IsValidFolder(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+            AssetDatabase.Refresh();
+        }
+
+        Mesh flatColoredMesh = new Mesh();
+        flatColoredMesh.vertices = mesh.vertices;
+        flatColoredMesh.triangles = mesh.triangles;
+        flatColoredMesh.uv = mesh.uv;
+        flatColoredMesh.normals = mesh.normals;
+        flatColoredMesh.colors = mesh.colors;
+        flatColoredMesh.tangents = mesh.tangents;
+
+        string materialPath = $"{folderPath}/{FLAT_COLORED}{mesh.name}.asset";
+        AssetDatabase.CreateAsset(flatColoredMesh, materialPath);
+
+        AssetDatabase.Refresh();
+
+        Debug.Log("new mesh created");
+        return flatColoredMesh;
     }
 
     public static Material GetSharedMaterial()
@@ -37,5 +107,25 @@ public static class FlatColorizerManager
             sharedMat = CreateMaterial();
 
         return sharedMat;
+    }
+
+    public static Texture2D GetTexture()
+    {
+        Texture2D texture = Resources.Load<Texture2D>("FlatColorizer/Material/Texture/Texture");
+        if (texture == null)
+            texture = CreateTexture();
+        return texture;
+    }
+
+    public static Mesh GetFlatColorizedMesh(Mesh mesh)
+    {
+        if (mesh.name.Contains(FLAT_COLORED))
+            return mesh;
+
+        Mesh flatColoredMesh = Resources.Load<Mesh>($"FlatColorizer/Mesh/{FLAT_COLORED}{mesh.name}.asset");
+        if (flatColoredMesh == null)
+            flatColoredMesh = CreateFlatColoredMesh(mesh);
+
+        return flatColoredMesh;
     }
 }
